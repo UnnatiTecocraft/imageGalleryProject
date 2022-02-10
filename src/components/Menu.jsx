3f2787f36b1110805e372b235eac9f1a55d25e10
UnnatiTecocraft <99247348+UnnatiTecocraft@users.noreply.github.com>
2022-02-10 15:01:33 +0530

    make whole project and commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const Menu = [
+    {
+        id: 1,
+        image: "Images/maggi3.jpg",
+        name: "Maggi",
+        category: "breakfast",
+        price: "12₹",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 2,
+        image: "Images/pakoda.jpg",
+        name: "Pakoda",
+        category: "evening",
+        price: "20₹",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 3,
+        image: "Images/corn.jpg",
+        name: "Corn",
+        category: "breakfast",
+        price: "15₹",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 4,
+        image: "Images/chole.jpg",
+        name: "Chole Bhature",
+        category: "lunch",
+        price: "50₹",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 5,
+        image: "Images/pizza2.jpg",
+        name: "Pizza",
+        category: "dinner",
+        price: "100₹",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 6,
+        image: "Images/non-veg-thali.jpg",
+        name: "Non-Veg Thali",
+        category: "dinner",
+        price: "180₹",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 7,
+        image: "Images/sweets.jpg",
+        name: "Sweets",
+        category: "dinner",
+        price: "60₹",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 8,
+        image: "Images/rajma-chawal.jpg",
+        name: "Rajma Chawal",
+        category: "lunch",
+        price: "80₹",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+        id: 9,
+        image: "Images/samosa.jpg",
+        name: "Samosa",
+        category: "evening",
+        price: "20₹",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },    
+];
+
+export default Menu;
